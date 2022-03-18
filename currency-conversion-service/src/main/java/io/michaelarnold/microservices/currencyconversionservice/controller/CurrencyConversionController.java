dbaee9a0b64c7824e6d90b3d0f3a68025ff8f04d
Michael Arnold <me@michaelarnold.io>
2022-03-18 12:14:10 -0500

    API Gateway

--- currency-conversion-service/src/main/java/io/michaelarnold/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/io/michaelarnold/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package io.michaelarnold.microservices.currencyconversionservice.controller;
 
 import io.michaelarnold.microservices.currencyconversionservice.bean.CurrencyConversion;
+import io.michaelarnold.microservices.currencyconversionservice.service.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,19 +16,16 @@ import java.util.Map;
 @RestController
 public class CurrencyConversionController {
 
+    @Autowired
+    private CurrencyExchangeProxy proxy;
+
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String from,
             @PathVariable String to,
             @PathVariable BigDecimal quantity) {
 
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
-        ResponseEntity<CurrencyConversion> response = new RestTemplate().getForEntity("http://localhost:8001/currency-exchange/from/{from}/to/{to}",
-                CurrencyConversion.class, uriVariables);
-
-        CurrencyConversion currencyConversion = response.getBody();
+        CurrencyConversion currencyConversion = proxy.getExchangeValue(from, to);
 
         return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
                 currencyConversion.getConversionMultiple(),
